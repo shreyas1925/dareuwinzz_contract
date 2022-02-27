9576a3eb8aba65d39184d5a4640ed79424aec5b8
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-02-28 00:26:10 +0530

    removed

--- contracts/dare.sol
+++ contracts/dare.sol
@@ -2,12 +2,10 @@ pragma solidity >=0.4.20;
 contract Lottery
 {
     address public manager;
+
     constructor() public {
         manager = msg.sender;
     }
     
-    function getManagerId() public view returns(uid)
-    {
-    
-    }
+   
 }
