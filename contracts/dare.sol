7f173fa98c1226f9f24cc99d25ee68f26186c392
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-03-01 23:33:35 +0530

    writing players function

--- contracts/dare.sol
+++ contracts/dare.sol
@@ -2,9 +2,17 @@ pragma solidity >=0.4.20;
 contract Lottery
 {
     address public manager;
+    address[] public players;
+
+    // msg is a global one it is available in any function envocation
+    // ex : msg.sender,msg.gas,msg.data etc
 
     constructor() public {
         manager = msg.sender;
+    }
+    // when someone call this function they send along some ether
+    function enter() public payable {
+        players.push(msg.sender)
     }
     
    
