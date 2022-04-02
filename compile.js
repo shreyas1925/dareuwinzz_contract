d349071baa2af714092c59a2ae7eb64fa1baeb54
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-04-02 19:17:34 +0530

    error resolved

--- compile.js
+++ compile.js
@@ -4,7 +4,7 @@ const solc = require("solc");
 const lotterpath = path.resolve(__dirname, "contracts", "Dare.sol");
 const source = fs.readFileSync(lotterpath, "utf8");
 
-// console.log(solc.compile(source, 1));
+console.log(solc.compile(source, 1));
 // console.log(source);
 
 module.exports = solc.compile(source, 1).contracts[":Dare"];
--- contracts/dare.sol
+++ contracts/dare.sol
@@ -1,12 +1,13 @@
-pragma solidity >=0.4.20;
-contract Lottery
+pragma solidity >=0.4.21 < 0.7.0;
+contract Dare
 {
     address public manager;
     address[] public players;
     // msg is a global one it is available in any function envocation
     // ex : msg.sender,msg.gas,msg.data etc
 
-    constructor() public {
+    constructor() public
+    {
         manager = msg.sender;
     }
     // when someone call this function they send along some ether
@@ -22,7 +23,7 @@ contract Lottery
 
    function pickaWinner() public restricted {
         uint idx = random()%players.length;
-        payable(players[idx]).transfer(address(this).balance);
+        players[idx].transfer(address(this).balance);
         players = new address[](0); // I am emptying the players array
     }
 
@@ -30,6 +31,7 @@ contract Lottery
         require(msg.sender == manager,"User not authorized to pick a winner");
         _;
     }
+
     function getAllPlayers() public view returns (address[] memory)
     {
         return players;
